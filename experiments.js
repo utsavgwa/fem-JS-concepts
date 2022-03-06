836db740f27c46fb4da314e0eefde359f6e3108a
Utsav <utsavgwa@gmail.com>
2022-03-06 11:56:41 +0530

    to declare string type in jS

--- experiments.js
+++ experiments.js
@@ -1,4 +1,8 @@
-const monthlyRent = 500;
+/*const monthlyRent = 500;
 
 const yearlyRent = monthlyRent * 12;
 console.log(yearlyRent);
+*/
+/* declared string in jS */
+const myName = "tis the season";
+console.log(myName);
