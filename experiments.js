f990ca12d8f878ae22ffff4b4a88163b5547eb8c
Utsav <utsavgwa@gmail.com>
2022-03-06 23:52:28 +0530

    using string type in jS

--- experiments.js
+++ experiments.js
@@ -1,19 +1,22 @@
+// we use slash slash to comment things out in javaScript
+
+
 /*const monthlyRent = 500;
 
 const yearlyRent = monthlyRent * 12;
 console.log(yearlyRent);
 */
-
-/* declared string in jS */
+ 
+//declared string in jS
 const myName = "tis the season";  
 console.log(myName);
 
-/* declare string using back tick */
+// declare string using back tick
 const firstName = "Brian";
 const lastName = "Holt";
 
 const sentence = "Hello " + firstName + " " + lastName + "! How are you!?";
-/** can do this with new template, introduced in 2015*/
+//can do this with new template, introduced in 2015
 const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
 
 console.log(sentence);	
