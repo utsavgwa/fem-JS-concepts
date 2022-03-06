bb6c8fec6f518d4144dd02ab85d6549525e95c00
Utsav <utsavgwa@gmail.com>
2022-03-06 23:46:32 +0530

    to declare string type in jS

--- experiments.js
+++ experiments.js
@@ -3,6 +3,18 @@
 const yearlyRent = monthlyRent * 12;
 console.log(yearlyRent);
 */
+
 /* declared string in jS */
-const myName = "tis the season";
+const myName = "tis the season";  
 console.log(myName);
+
+/* declare string using back tick */
+const firstName = "Brian";
+const lastName = "Holt";
+
+const sentence = "Hello " + firstName + " " + lastName + "! How are you!?";
+/** can do this with new template, introduced in 2015*/
+const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
+
+console.log(sentence);	
+console.log(sentenceWithTemplate);
