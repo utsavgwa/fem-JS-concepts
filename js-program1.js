4fcbc140b5fa3a849628f2e12baf2c4adb3f39de
Utsav <utsavgwa@gmail.com>
2022-03-13 00:22:07 +0530

    document variable and querySelectorAll method in jS

--- js-program1.js
+++ js-program1.js
@@ -2,5 +2,13 @@
 // const yearlyRent = monthlyRent * 12;
 // console.log(yearlyRent);
 
-const redSquare = document.querySelector('.red-square');
+const redSquare = document.querySelector('.red-square');//gave CSS selector to querySelector, and it gives the first instance of it on the page
 redSquare.style.backgroundColor = 'limegreen';
+// here we called a method on document. document is a globally available variable in the browser that you use to interact with the HTML and CSS.
+
+// here querySelectorAll gives back an array of all elements, that have .js-target class
+const elementsToChange = document.querySelectorAll('.js-target');
+for (let i = 0; i < elementsToChange.length; i++) {
+  const currentElement = elementsToChange[i];
+  currentElement.innerText = "Modified by JavaScript!";
+}
