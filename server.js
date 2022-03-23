f64a927367f77bd508b5e30e00e17e4b4b59a4aa
Utsav <utsavgwa@gmail.com>
2022-03-23 14:12:05 +0530

    created basic server using node.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+// library inside of node for handling connections
 const http = require("http");
 
+//created server function, that runs every single time
 const server = http.createServer(function(req, res) {
   console.log(`user visited ${req.url}`);
   res.end("hello!");
 });
-
+// localhost is an alias for your own computer
+// :3000 is the port to connect on 
 console.log("listening on http://localhost:3000");
 server.listen(3000);
