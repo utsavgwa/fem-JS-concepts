65f39bd44821ca0ab6ace2b7937780c82b75c963
Utsav <utsavgwa@gmail.com>
2022-03-25 23:37:46 +0530

    added comments

--- server1.js
+++ server1.js
@@ -1,7 +1,7 @@
 // how you create a new server.
 const express = require("express");
 
-const app = express();
+const app = express();//run the function, you get app(one server)
 
 // how you handle a new route. A route is how you handle the request when a user hits a particular URL
 app.get("/", function(req, res) {
